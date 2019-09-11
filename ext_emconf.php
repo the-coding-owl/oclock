d199448b12942ccbaed39440838b22672e5672d0
Kevin Ditscheid <kevin.ditscheid@sgalinski.de>
2019-09-12 00:07:45 +0200

    [BUGFIX] Fix TYPO3 dependency version

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.0.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.5'
+            'typo3' => '9.5.9-9.5.99'
         ),
         'conflicts' => array(
         ),
