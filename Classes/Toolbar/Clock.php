16e1ebe3ee6676ff279dc17661ba878463a11f3d
Kevin Ditscheid <kevin.ditscheid@sgalinski.de>
2019-09-12 22:26:09 +0200

    [TASK] Add timezone display in dropdown

--- Classes/Toolbar/Clock.php
+++ Classes/Toolbar/Clock.php
@@ -4,11 +4,18 @@ namespace TheCodingOwl\Oclock\Toolbar;
 use TYPO3\CMS\Backend\Toolbar\ToolbarItemInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Localization\LanguageService;
 
 class Clock implements ToolbarItemInterface {
+    /**
+     * @var LanguageService
+     */
+    protected $languageService;
+
     public function __construct() {
         $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
         $pageRenderer->loadRequireJsModule('TYPO3/CMS/Oclock/Clock');
+        $this->languageService = GeneralUtility::makeInstance(LanguageService::class);
     }
 
     public function checkAccess() {
@@ -16,7 +23,7 @@ class Clock implements ToolbarItemInterface {
     }
 
     public function getItem() {
-        return '<span class="tx_oclock_time" data-time="' . (new \DateTime())->format('r') . '"></span>';
+        return '<span class="server-time"></span>';
     }
 
     public function hasDropDown() {
@@ -24,11 +31,18 @@ class Clock implements ToolbarItemInterface {
     }
 
     public function getDropDown() {
-        return '';
+        return '<p>' . $this->languageService->sL('LLL:EXT:oclock/Resources/Private/Language/locallang.xlf:toolbar.timezone.server') . ': <span  class="server-timezone">' . (new \DateTime())->format('e') . '</span></p>'
+            . '<p>' . $this->languageService->sL('LLL:EXT:oclock/Resources/Private/Language/locallang.xlf:toolbar.time.server') . ': <span class="server-time">' . '</span></p>'
+            . '<hr />'
+            . '<p>' . $this->languageService->sL('LLL:EXT:oclock/Resources/Private/Language/locallang.xlf:toolbar.timezone.browser') . ': <span class="browser-timezone"></span></p>'
+            . '<p>' . $this->languageService->sL('LLL:EXT:oclock/Resources/Private/Language/locallang.xlf:toolbar.time.browser') . ': <span class="browser-time"></span></p>';
     }
 
     public function getAdditionalAttributes() {
-        return '';
+        return [
+            'class' => 'tx_oclock',
+            'data-time' => (new \DateTime())->format('r')
+        ];
     }
 
     public function getIndex() {
--- Resources/Public/JavaScript/Clock.js
+++ Resources/Public/JavaScript/Clock.js
@@ -1,44 +1,74 @@
-define([],function(){
+define(['jquery'],function($){
     let Clock = {
-        selector: '.tx_oclock_time',
+        selector: '.tx_oclock',
+        serverTimeSelector: '.server-time',
+        browserTimeSelector: '.browser-time',
+        serverTimeZoneSelector: '.server-timezone',
+        browserTimeZoneSelector: '.browser-timezone',
         init: function() {
-            let element = document.querySelectorAll(this.selector);
-            for(let index in element) {
-                if (typeof element[index] !== 'object') {
-                    continue;
-                }
-
-                let timestamp = element[index].dataset.time,
-                    date = new Date(timestamp);
-                    spans = {
-                        hour: document.createElement('SPAN'),
-                        minute: document.createElement('SPAN'),
-                        second: document.createElement('SPAN')
-                    },
-                    hours = date.getHours(),
-                    minutes = date.getMinutes(),
-                    seconds = date.getSeconds();
-                spans.hour.innerText = hours < 10 ? '0' + hours : hours;
-                spans.minute.innerText = minutes < 10 ? '0' + minutes : minutes;
-                spans.second.innerText = seconds < 10 ? '0' + seconds : seconds;
-                spans.hour.classList.add('hour');
-                spans.minute.classList.add('minute');
-                spans.second.classList.add('second');
-                element[index].appendChild(spans.hour);
-                element[index].appendChild(document.createTextNode(':'));
-                element[index].appendChild(spans.minute);
-                element[index].appendChild(document.createTextNode(':'));
-                element[index].appendChild(spans.second);
-                setInterval(function(){
-                    date.setSeconds(date.getSeconds() + 1);
-                    let hours = date.getHours(),
-                        minutes = date.getMinutes(),
-                        seconds = date.getSeconds();
-                    spans.hour.innerText = hours < 10 ? '0' + hours : hours;
-                    spans.minute.innerText = minutes < 10 ? '0' + minutes : minutes;
-                    spans.second.innerText = seconds < 10 ? '0' + seconds : seconds;
-                }, 1000);
-            }
+            let clock = this,
+                containers = $(clock.selector);
+            containers.each(function(){
+                let container = $(this),
+                    elements = container.find(clock.serverTimeSelector),
+                    date = new Date(container.data('time'));
+                clock.setTimeZone(date, container.find(clock.serverTimeZoneSelector));
+                elements.each(function() {
+                    let element = $(this),
+                        spans = clock.createTimeSpans()
+                        date = new Date(container.data('time'));
+                    clock.updateTime(date, spans);
+                    clock.appendTimes(element, spans);
+                    clock.setClock(date, spans);
+                });
+                let browserElements = container.find(clock.browserTimeSelector);
+                date = new Date();
+                clock.setTimeZone(date, container.find(clock.browserTimeZoneSelector));
+                browserElements.each(function () {
+                    let element = $(this),
+                        spans = clock.createTimeSpans()
+                        date = new Date();
+                    clock.updateTime(date, spans);
+                    clock.appendTimes(element, spans);
+                    clock.setClock(date, spans);
+                });
+            });
+        },
+        updateTime: function(date, spans) {
+            let hours = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds();
+            spans.hour.innerText = hours < 10 ? '0' + hours : hours;
+            spans.minute.innerText = minutes < 10 ? '0' + minutes : minutes;
+            spans.second.innerText = seconds < 10 ? '0' + seconds : seconds;
+        },
+        createTimeSpans: function() {
+            let spans = {
+                hour: document.createElement('SPAN'),
+                minute: document.createElement('SPAN'),
+                second: document.createElement('SPAN')
+            };
+            spans.hour.classList.add('hour');
+            spans.minute.classList.add('minute');
+            spans.second.classList.add('second');
+            return spans;
+        },
+        setClock: function(date, spans) {
+            let clock = this;
+            setInterval(function(){
+                date.setSeconds(date.getSeconds() + 1);
+                clock.updateTime(date, spans);
+            }, 1000);
+        },
+        appendTimes: function(element, spans) {
+            element.append(spans.hour);
+            element.append(document.createTextNode(':'));
+            element.append(spans.minute);
+            element.append(document.createTextNode(':'));
+            element.append(spans.second);
+        },
+        setTimeZone: function(date, element) {
+            element.text(/\((.*)\)/.exec(date.toString())[1]);
         }
     };
     Clock.init();
