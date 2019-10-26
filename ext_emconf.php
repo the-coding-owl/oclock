f2e825e9a2aa68530b98ac63d0fd6c314de5557f
Kevin Ditscheid <kevin@the-coding-owl.de>
2019-10-26 21:56:13 +0200

    [TASK] Prepare TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.0.3',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.5.9-9.5.99'
+            'typo3' => '9.5.9-10.1.99'
         ),
         'conflicts' => array(
         ),
