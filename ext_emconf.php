40b4e7f898467bba448c86dc7a67a1ade78cdefa
Kevin Ditscheid <kevin@the-coding-owl.de>
2019-12-19 21:27:44 +0100

    [TASK] Add TYPO3 10.2 to dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,7 +28,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.0.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.5.9-10.1.99'
+            'typo3' => '9.5.9-10.2.99'
         ),
         'conflicts' => array(
         ),
