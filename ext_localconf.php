050abe9b6e58edeabb9f6b328650f29082664929
Kevin Ditscheid <kevin@the-coding-owl.de>
2021-11-05 22:20:42 +0100

    [TASK] Use new Icon API

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,19 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
 call_user_func(function($extKey) {
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = \TheCodingOwl\Oclock\Toolbar\Clock::class;
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'the-coding-owl-clock',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        [
-            'source' => 'EXT:oclock/Resources/Public/Icons/clock.svg'
-        ]
-    );
+    if (version_compare(VersionNumberUtility::getCurrentTypo3Version(), '11.4.0', '<')) {
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Imaging\IconRegistry::class
+        );
+        $iconRegistry->registerIcon(
+            'the-coding-owl-clock',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            [
+                'source' => 'EXT:oclock/Resources/Public/Icons/clock.svg'
+            ]
+        );
+    }
 }, 'oclock');
